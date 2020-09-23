39edd84936a8ed5ca0598d98f310f9b5807153dc
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-23 10:41:06 +0530

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -28,12 +28,12 @@ variable "credentials" {
 variable "service_list" {
     type = list
     default = [
-	  "cloudresourcemanager.googleapis.com",  # Creates, reads, and updates metadata for Google Cloud Platform resource containers
+      "cloudresourcemanager.googleapis.com",  # Creates, reads, and updates metadata for Google Cloud Platform resource containers
       "serviceusage.googleapis.com",  # Enables services that service consumers want to use on Google Cloud Platform
       "logging.googleapis.com",  # Writes log entries and manages your Cloud Logging configuration
-      "admin.googleapis.com",
+      "admin.googleapis.com",  #  Lets administrators of project to view and manage resources like user, groups etc.
       "appengine.googleapis.com",  # Provisions and manages developers' App Engine applications
       "storage-api.googleapis.com",  # Lets you store and retrieve potentially-large, immutable data objects
-	  "containerregistry.googleapis.com",  # Provides secure, private Docker image storage on Google Cloud Platform
+      "containerregistry.googleapis.com",  # Provides secure, private Docker image storage on Google Cloud Platform
   ]
 }
