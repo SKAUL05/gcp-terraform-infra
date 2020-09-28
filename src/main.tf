68437b59a8f0462a0059786d9d70c0e49c0e8e2e
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-28 13:25:15 +0000

    Documented TF Code

--- src/main.tf
+++ src/main.tf
@@ -8,6 +8,7 @@ provider "google" {
   region  = var.region
 }
 
+# Enabling GCP Services
 resource "google_project_service" "enable_services" {
   project = var.project
   count   = length(var.service_list)
@@ -16,6 +17,7 @@ resource "google_project_service" "enable_services" {
   disable_dependent_services = true
 }
 
+# Creating Google App Engine Application
 resource "google_app_engine_application" "app" {
   depends_on = [google_project_service.enable_services]
   location_id = var.app_engine_location
