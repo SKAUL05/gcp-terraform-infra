87163c2fa42bb690c2009285c526a27be0ddec9a
Sarath Kaul <kaul.sarath@gmail.com>
2020-09-28 16:16:30 +0000

    Comments Added

--- src/main.tf
+++ src/main.tf
@@ -14,8 +14,8 @@ resource "google_project_service" "enable_services" {
   count   = length(var.service_list)
   service = var.service_list[count.index]
 
-  disable_dependent_services = true
-  disable_on_destroy = false
+  disable_dependent_services = true # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed
+  disable_on_destroy = false # If false, will not disable the service when the terraform resource is destroyed
 }
 
 # Creating Google App Engine Application
