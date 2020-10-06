7881dbc8c982351811c17c66c0f6f2e0e36569f0
Sarath Kaul <kaul.sarath@gmail.com>
2020-10-06 22:46:03 +0530

    Update variables.tf

--- src/variables.tf
+++ src/variables.tf
@@ -29,9 +29,9 @@ variable "service_list" {
    type = list
    default = [
      "cloudresourcemanager.googleapis.com", # Creates, reads, and updates metadata for Google Cloud Platform resource containers
-     "containerregistry.googleapis.com",    # Provides secure, private Docker image storage on Google Cloud Platform   
+     "containerregistry.googleapis.com",    # Provides secure, private Docker image storage on Google Cloud Platform
      "serviceusage.googleapis.com",         # Enables services that service consumers want to use on Google Cloud Platform
-     "storage-api.googleapis.com",          # Lets you store and retrieve potentially-large, immutable data objects   
+     "storage-api.googleapis.com",          # Lets you store and retrieve potentially-large, immutable data objects
      "appengine.googleapis.com",            # Provisions and manages developers' App Engine applications
      "logging.googleapis.com",              # Writes log entries and manages your Cloud Logging configuration
      "admin.googleapis.com",                #  Lets administrators of project to view and manage resources like user, groups etc.
