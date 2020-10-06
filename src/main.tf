061eddec48b583401194d46ac19e00de54b8f7dd
Sarath Kaul <kaul.sarath@gmail.com>
2020-10-06 15:08:56 +0000

    TF Code Formatted

--- src/main.tf
+++ src/main.tf
@@ -4,8 +4,8 @@ terraform {
 
 provider "google" {
   credentials = var.credentials
-  project = var.project
-  region  = var.region
+  project     = var.project
+  region      = var.region
 }
 
 # Enabling GCP Services
@@ -14,12 +14,12 @@ resource "google_project_service" "enable_services" {
   count   = length(var.service_list)
   service = var.service_list[count.index]
 
-  disable_dependent_services = true # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed
-  disable_on_destroy = false # If false, will not disable the service when the terraform resource is destroyed
+  disable_dependent_services = true  # If true, services that are enabled and which depend on this service should also be disabled when this service is destroyed
+  disable_on_destroy         = false # If false, will not disable the service when the terraform resource is destroyed
 }
 
 # Creating Google App Engine Application
 resource "google_app_engine_application" "app" {
-  depends_on = [google_project_service.enable_services]
+  depends_on  = [google_project_service.enable_services]
   location_id = var.app_engine_location
 }
--- src/variables.tf
+++ src/variables.tf
@@ -1,11 +1,11 @@
 variable "project" {
   description = "GCP Project ID"
-  type = string
+  type        = string
 }
 
 variable "bucket" {
   description = "Name of bucket used for storing terraform state"
-  type = string
+  type        = string
 }
 
 variable "region" {
@@ -22,18 +22,18 @@ variable "zone" {
 
 variable "credentials" {
   description = "Credentials to be used for creating infrastructure"
-  type = string
+  type        = string
 }
 
 variable "service_list" {
-    type = list
+    type    = list
     default = [
       "cloudresourcemanager.googleapis.com",  # Creates, reads, and updates metadata for Google Cloud Platform resource containers
-      "serviceusage.googleapis.com",  # Enables services that service consumers want to use on Google Cloud Platform
-      "logging.googleapis.com",  # Writes log entries and manages your Cloud Logging configuration
-      "admin.googleapis.com",  #  Lets administrators of project to view and manage resources like user, groups etc.
-      "appengine.googleapis.com",  # Provisions and manages developers' App Engine applications
-      "storage-api.googleapis.com",  # Lets you store and retrieve potentially-large, immutable data objects
-      "containerregistry.googleapis.com",  # Provides secure, private Docker image storage on Google Cloud Platform
+      "containerregistry.googleapis.com",     # Provides secure, private Docker image storage on Google Cloud Platform      
+      "serviceusage.googleapis.com",          # Enables services that service consumers want to use on Google Cloud Platform
+      "storage-api.googleapis.com",           # Lets you store and retrieve potentially-large, immutable data objects      
+      "appengine.googleapis.com",             # Provisions and manages developers' App Engine applications      
+      "logging.googleapis.com",               # Writes log entries and manages your Cloud Logging configuration
+      "admin.googleapis.com",                 #  Lets administrators of project to view and manage resources like user, groups etc.
   ]
 }
